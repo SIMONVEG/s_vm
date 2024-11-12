96bd47055a414799e14532db7bfdc55802578924
SIMONVEG <simonvega2022@gmail.com>
2024-11-11 22:38:35 -0500

    yeah

---
+++ prueba/Principal.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Importancia del Agua en Panamá</title>
+    <script src="Principal.js"></script>
+    <link rel="stylesheet" href="Principal.css">
+</head>
+<body>
+  <?php include 'BarradeNavegacionP.html'; ?>
+<section class="section">
+  <div class="aline">
+  <?php include 'comentarios.php'; ?>
+</div>
+  <?php include 'FotterP.html'; ?>
+</section>
+</body>
+</html>
